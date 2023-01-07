244d967c5306be6c24fd856343d8b143582c364d
amadrafi <ahmadrafi0814@gmail.com>
2023-01-07 21:55:19 +0800

    update meta location

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,14 @@ import Navbar from '../shared/navbar'
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Project Bracket</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='description' content='Project Bracket' />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </>
--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,6 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.bgColor}>
-      <Head>
-        <title>Project Bracket</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta name='description' content='Project Bracket' />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className={styles.relativePos}>
         
         <div className={[ styles.jumbotron].join(" ")}>
