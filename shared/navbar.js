c2066b04a5254293aeb598059e5bb5c36ebd0ebe
amadrafi <ahmadrafi0814@gmail.com>
2023-01-10 23:02:31 +0700

    mobile version

--- shared/navbar.js
+++ shared/navbar.js
@@ -1,16 +1,37 @@
 import Link from "next/link"
 import styles from "../styles/navbar.module.css"
+import { useState } from "react"
+import { Divide as Hamburger } from "hamburger-react"
 
-const Navbar = () => {
+const DesktopNavbar = () => {
   return (
     <div className={[styles.navbar, styles.container].join(" ")}>
       <Link href='/'><span className={styles.bracketThe}>The </span><span className={styles.bracketTitle}>Bracket</span></Link>
       <ul>
-        <Link className={styles.navLinks} href='/comingSoon'>Our goal</Link>
-        <Link className={styles.navLinks} href='/comingSoon'>About us</Link>
+        {/* <Link className={styles.navLinks} href='/comingSoon'>Our goal</Link>
+        <Link className={styles.navLinks} href='/comingSoon'>About us</Link> */}
         <Link className={[styles.navButton, styles.navLinks].join(" ")} href='/comingSoon'>Contact us</Link>
       </ul>
     </div>
+  )
+}
+
+const MobileNavbar = (props) => {
+  const { active, toggle } = props
+  return (
+    <>
+    </>
+  )
+}
+
+const Navbar = (props) => {
+  const [navbarState, setNavbarState] = useState(false)
+  const {isMobile} = props;
+  return (
+    isMobile?
+    <MobileNavbar active={navbarState} toggle={setNavbarState} />
+    :
+    <DesktopNavbar />
   )
 }
 
