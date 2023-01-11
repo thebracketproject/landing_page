a9b6ac79610f89e77b69c92825f8088db89d6010
amadrafi <ahmadrafi0814@gmail.com>
2023-01-11 12:15:34 +0700

    few meta changes

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,16 @@ import Head from 'next/head'
 import '../styles/globals.css'
 import Navbar from '../shared/navbar'
 import Footer from '../shared/footer'
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ParallaxProvider>
       <Head>
         <title>The Bracket Project</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <meta name='keywords' content='Bracket, bracket, the bracket, bracket project, edinburgh bracket' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='description' content='Project Bracket' />
         <link rel="icon" href="/favicon.ico" />
@@ -17,6 +19,6 @@ export default function App({ Component, pageProps }) {
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </ParallaxProvider>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,26 @@
-import Head from 'next/head'
+import { Parallax } from 'react-scroll-parallax'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.bgColor}>
-      <div className={styles.relativePos}>
         
-        <div className={[ styles.jumbotron].join(" ")}>
-          <div className={styles.slantedSquare} >
-            <p>Experience new ways to connect at university</p>
-          </div>
-          <div className={styles.slantedSquare2} />
+      <div className={[ styles.jumbotron].join(" ")}>
+        <div className={styles.slantedSquare} >
+          <p>Experience new ways to connect at university</p>
         </div>
-
-        <div className={[styles.mainBody, styles.container].join(" ")}>
-          <div className={styles.contentHeader}>
-            <p><span className={styles.bracketThe}>The </span><span className={styles.bracketTitle}>Bracket </span><span className={styles.bracketSubtext}> ?</span></p>
-            <p className={styles.jungeText}>Everything a student needs to unlock their potential</p>
-            <p className={styles.descText}>Connect with like-minded university students and build exclusive friend groups with Bracket, the social platform designed for you! Discover new opportunities and create meaningful connections based on your individual interests and personality.</p>
-          </div>
-        </div>
-        
+        <div className={styles.slantedSquare2} />
       </div>
+
+      <div className={[styles.mainBody, styles.container].join(" ")}>
+        <div className={styles.contentHeader}>
+          <p><span className={styles.bracketThe}>The </span><span className={styles.bracketTitle}>Bracket </span><span className={styles.bracketSubtext}> ?</span></p>
+          <p className={styles.jungeText}>Everything a student needs to unlock their potential</p>
+          <p className={styles.descText}>Connect with like-minded university students and build exclusive friend groups with Bracket, the social platform designed for you! Discover new opportunities and create meaningful connections based on your individual interests and personality.</p>
+        </div>
+      </div>
+
+
     </div>
   )
 }
